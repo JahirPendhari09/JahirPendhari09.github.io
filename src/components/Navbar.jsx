5f56aa70c2a6e3a30302918ed52f27743b3ae167
JahirPendhari <jahirpp1999@gmail.com>
2023-09-28 00:03:29 +0530

    update

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import {Link} from "react-router-dom"
 const Navbar=()=>{
     return <div>
-        <nav id="nav-menu">
-         <ul>
-           <li><a href="#home">Home</a></li>
-           <li><a href="#about">About</a></li>
-           <li><a href="#skills">Skills</a></li>
-           <li><a href="#projects">Projects</a></li>
-           <li><a href="#contact">Contact</a></li>
+        <nav id="nav-menu" >
+         <ul style={{display:"flex", justifyContent:"space-around"}}>
+            <li><a className="nav-link home" href="#home">Home</a></li>
+            <li><a className="nav-link about" href="#about">About</a></li>
+            <li><a className="nav-link skills" href="#skills">Skills</a></li>
+            <li><a className="nav-link projects" href="#projects">Projects</a></li>
+            <li><a className="nav-link contact" href="#contact">Contact</a></li>
          </ul>
       </nav>
     </div>
