6a76aa2f9a13cc842a29ce1be86fb0d8e750c931
JahirPendhari <jahirpp1999@gmail.com>
2023-10-05 07:53:08 +0530

    navbar inProcess

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,65 @@
-import {Link} from "react-router-dom"
+import styled from "styled-components"
 const Navbar=()=>{
-    return <div>
-        <nav id="nav-menu" >
-         <ul style={{display:"flex", justifyContent:"space-around"}}>
-            <li><a className="nav-link home" href="#home">Home</a></li>
-            <li><a className="nav-link about" href="#about">About</a></li>
-            <li><a className="nav-link skills" href="#skills">Skills</a></li>
-            <li><a className="nav-link projects" href="#projects">Projects</a></li>
-            <li><a className="nav-link contact" href="#contact">Contact</a></li>
-         </ul>
-      </nav>
-    </div>
+    return <DIV>
+        <div id="nav-profile">
+            <div>
+               <p>JP</p>
+            </div>
+        </div>
+        <div id="nav-menu" >
+            <p><a className="nav-link home" href="#">Home</a></p>
+            <p><a className="nav-link about" href="#about">About</a></p>
+            <p><a className="nav-link skills" href="#skills">Skills</a></p>
+            <p><a className="nav-link projects" href="#projects">Projects</a></p>
+            <p><a className="nav-link contact" href="#contact">Contact</a></p>
+        </div>
+    </DIV>
 }
 
 export {Navbar}
+
+const DIV=styled.div`
+    width: 100%;
+    display: flex;
+    justify-content: space-between;
+    margin:auto;
+    height: 80px;
+    background-color:black;
+    color: white;
+    align-items: center;
+    box-shadow: rgba(0, 0, 0, 0.09) 0px 2px 1px, rgba(0, 0, 0, 0.09) 0px 4px 2px, rgba(0, 0, 0, 0.09) 0px 8px 4px, rgba(13, 20, 243, 0.09) 0px 16px 8px, rgba(0, 0, 0, 0.09) 0px 32px 16px;
+
+
+    #nav-profile{
+        width: 100%;
+        margin-left:100px;
+    }
+    #nav-profile > div{
+        width: 60px;
+        height: 60px;
+        border: 1px solid green;
+        border-radius:50%;
+        background-color:green;
+        padding-top:10px;
+        cursor: pointer;
+    }
+    #nav-profile > div > p{
+        /* margin-bottom:10px; */
+        font-size: 25px;
+        font-weight:bold;
+    }
+    #nav-menu{
+        width: 100%;
+        display: flex;
+        gap: 50px;
+        font-size: 20px;
+        font-weight: bold;
+    }
+    a{
+        text-decoration:none;
+        color: #0af30a;
+    }
+    a:hover{
+        color:orange;
+    }
+`
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,78 @@
+import styled from "styled-components";
+import profilePic from "../Images/Jahir_Pic.png"
+import { Link } from "@chakra-ui/react";
+import resume from "../Images/resume.png"
 const Home=()=>{
     return (
-        <div id="home">
-          <h2>Home</h2>
-          <p>Hello My name is Jahir Pendhari</p>
+     <DIV>
+          <div id="home" className="mainDiv">
+          <div className="div-1">
+            <img src={profilePic} alt="My Photo"/>
+          </div>
+          <div className="div-2">
+            <p style={{fontSize:"35px",fontWeight:"bold"}}>Hello 👋</p>
+            <p>
+                Welcome to my portfolio, showcasing my expertise in full stack web development. Trained by Masai School, I have a strong command of the MERN stack, API integrations, and responsive design. I have successfully created diverse projects that blend functionality with aesthetics, demonstrating collaborative teamwork and meticulous attention to details. Step into my world of code and innovation, where each line is crafted to leave a lasting impact.
+            </p>
+            <p style={{margin:"20px"}}><Link
+               className="resume"
+               href={resume}
+               download={true}
+               style={{
+                padding:"10px",
+                borderRadius:"10px",
+                backgroundColor:"green",
+                textDecoration:"none",
+               }}
+               target="_blank"
+               textAlign="center"
+               onClick={() => {
+               window.open(
+                 "https://drive.google.com/file/d/1PKOfb7E58nr4_5XTVaK4e_9dk7JfQw8y/view?usp=sharing",
+                 "_blank",
+                 "noreferrer"
+                );
+               }}
+             >
+              Resume
+            </Link>
+            </p>
+          </div>
         </div>
+     </DIV>
       );
 }
 
 export {Home}
+
+const DIV=styled.div`
+  .mainDiv{
+    width: 90%;
+    display: flex;
+    justify-content: space-between;
+    border-radius: 20px;
+    margin:auto;
+    margin-top:50px;
+    padding: 20px;
+    background-color:black;
+    border: 1px solid blue;
+    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px, rgba(9, 82, 200, 0.3) 0px 8px 16px -8px;
+    cursor: pointer;
+  }
+  .div-1{
+    width: 50%;
+    margin:20px;
+  }
+  .div-2{
+    width: 100%;
+    text-align:left;
+    font-size: 20px;
+    color:white;
+    margin-top:30px;
+  }
+  img{
+    width: 90%;
+    height: 100%;
+    border-radius:10px;
+  }
+`
