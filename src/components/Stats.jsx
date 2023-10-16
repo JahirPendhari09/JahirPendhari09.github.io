42c1b7165365ba1a9ab96f67bbfa2a38164696f9
JahirPendhari <jahirpp1999@gmail.com>
2023-10-16 20:41:19 +0530

    process

--- src/components/Stats.jsx
+++ src/components/Stats.jsx
@@ -6,13 +6,13 @@ const Stats = () => {
   return (
     // streak
     <div style={{ width: "100%", margin: "auto auto 3rem auto" }}
-     id="github-streak-stats"
+     
     >
       <div
         style={{ width: "40%", margin: "3rem auto" }}
       >
         <a href="https://github.com/JahirPendhari09">
-          <img
+          <img id="github-streak-stats"
             src="https://github-readme-streak-stats.herokuapp.com?user=JahirPendhari09"
             alt="i_mg1"
           />
@@ -22,20 +22,20 @@ const Stats = () => {
       <div
         style={{display:"flex", width: "60%",margin: "auto",justifyContent:"center",alignItems:"center",gap:"3rem"}}
        >
-        <div id="github-top-langs">
+        <div >
           <a href="https://github.com/JahirPendhari09">
-            <img
+            <img id="github-top-langs"
               src="https://github-readme-stats.vercel.app/api/top-langs/?username=JahirPendhari09" 
               alt="i_mg2"
             />
           </a>
         </div>
-        <div id="github-stats-card">
+        <div >
           <a
             style={{ height: "100%" }}
             href="https://github.com/JahirPendhari09"
           >
-            <img
+            <img id="github-stats-card"
               align="left"
               src="https://github-readme-stats.vercel.app/api?username=JahirPendhari09&count_private=true&show_icons=true" 
               alt="i_mg3"
